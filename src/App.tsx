e9420260cf98c8f4fa7a291475af9c840b59c064
dfirthk <dfirthk@wgu.edu>
2024-10-29 14:08:26 -0700

    Init Commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from './components/ui/button';
+
+function App() {
+	return (
+		<Button variant={'solid'} colorPalette={'blue'}>
+			Button
+		</Button>
+	);
+}
+
+export default App;
