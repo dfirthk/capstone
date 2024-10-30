571a77e43fe850f3bc5207dba166251a5b422d78
dfirthk <dfirthk@wgu.edu>
2024-10-30 06:59:23 -0700

    Implement Dark Mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import { Theme } from '@chakra-ui/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
@@ -7,7 +8,9 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
 		<Provider>
-			<App />
+			<Theme appearance="dark">
+				<App />
+			</Theme>
 		</Provider>
 	</React.StrictMode>
 );
